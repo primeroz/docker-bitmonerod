e7ded37cb59628ef9e6ef9abeecd00652f9ed953
Levin Keller <levin@coyno.com>
2015-07-27 15:08:15 +0200

    Logging number of cores in build.

--- buildAndInstallMonero.sh
+++ buildAndInstallMonero.sh
@@ -3,4 +3,5 @@ git clone https://github.com/monero-project/bitmonero.git
 cd bitmonero
 git checkout v0.8.8.6
 NUM_CORES="$(grep -c ^processor /proc/cpuinfo)"
+echo $('Building with ' $NUM_CORES ' cores')
 make -j $NUM_CORES
